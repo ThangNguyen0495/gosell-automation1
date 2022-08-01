be0e6543b5b9ccff240a425af33982a3bb2b56bf
Tien Van <minhtienxoxo1@gmail.com>
2022-08-01 17:44:49 +0700

    Added UICommonAction.java

---
+++ src/main/java/utilities/UICommonAction.java
@@ -0,0 +1,57 @@
+package utilities;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class UICommonAction {
+	
+	final static Logger logger = LogManager.getLogger(UICommonAction.class);
+	
+	WebDriver driver;
+	WebDriverWait wait;
+
+	public UICommonAction(WebDriver driver) {
+		this.driver = driver;
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	}
+
+	public void clickElement(WebElement element) {
+		try {
+			wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+		} catch (StaleElementReferenceException ex) {
+			System.out.println("Catch StaleElementReferenceException in clickElement");
+			wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+		}
+	}
+	
+	public void inputText(WebElement element, String text) {
+		try {
+			wait.until(ExpectedConditions.elementToBeClickable(element)).clear();
+			element.sendKeys(text);
+		} catch (StaleElementReferenceException ex) {
+			System.out.println("Catch StaleElementReferenceException in clickElement");
+			wait.until(ExpectedConditions.elementToBeClickable(element)).clear();
+			element.sendKeys(text);
+		}
+	}
+	
+	public String getText(WebElement element) {
+		String text;
+		try {
+			text = wait.until(ExpectedConditions.visibilityOf(element)).getText();
+		} catch (StaleElementReferenceException ex) {
+			System.out.println("Catch StaleElementReferenceException in getText");
+			text = wait.until(ExpectedConditions.visibilityOf(element)).getText();
+		}
+		System.out.println("Text get" + text);
+		return text;
+	}	
+
+}
