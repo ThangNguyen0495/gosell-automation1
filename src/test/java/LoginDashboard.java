7ad0a91d0d5a1d2f3565858ca84e73b1627f0952
Tien Van <minhtienxoxo1@gmail.com>
2022-08-05 18:14:57 +0700

    Updated TCs

--- src/test/java/LoginDashboard.java
+++ src/test/java/LoginDashboard.java
@@ -1,17 +1,39 @@
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.dashboard.LoginPage;
 import pages.dashboard.home.HomePage;
-
-import static utilities.account.AccountTest.*;
-import static utilities.message.Message.*;
+import utilities.jsonFileUtility;
 
 public class LoginDashboard extends BaseTest {
+	
+    String MAIL = "qcgosell01@gmail.com";
+    String PASSWORD = "Psso12!@";
+    String PHONE = "0363530754";
+    String PHONE_PASSWORD = "Abc@12345";
+    
+    String BLANK_ERROR;
+    String INVALID_MAIL_ERROR;
+    String INVALID_PHONE_ERROR;
+    String INVALID_CREDENTIALS_ERROR;
+	
+    @BeforeClass
+    public void readData() {
+    	try {
+			BLANK_ERROR = jsonFileUtility.readJsonFile("DBInfo.json").findValue("dashboard").findValue("emptyError").asText();
+			INVALID_MAIL_ERROR = jsonFileUtility.readJsonFile("DBInfo.json").findValue("dashboard").findValue("invalidMailFormat").asText();
+			INVALID_PHONE_ERROR = jsonFileUtility.readJsonFile("DBInfo.json").findValue("dashboard").findValue("invalidPhoneFormat").asText();
+			INVALID_CREDENTIALS_ERROR = jsonFileUtility.readJsonFile("DBInfo.json").findValue("dashboard").findValue("invalidCredentials").asText();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }	    
+    
     @Test
     public void TC01_DB_LoginWithAllFieldsLeftBlank() {
         new LoginPage(driver).navigate()
                 .performLogin("", "")
-                .verifyEmailOrPhoneNumberError(BLANK_ERROR_MESSAGE)
-                .verifyPasswordError(BLANK_ERROR_MESSAGE)
+                .verifyEmailOrPhoneNumberError(BLANK_ERROR)
+                .verifyPasswordError(BLANK_ERROR)
                 .completeVerify();
     }
 
@@ -19,7 +41,7 @@ public class LoginDashboard extends BaseTest {
     public void TC02_DB_LoginWithInvalidPhoneFormat() {
         new LoginPage(driver).navigate()
                 .performLogin("123", PASSWORD)
-                .verifyEmailOrPhoneNumberError(INVALID_PHONE_ERROR_MESSAGE)
+                .verifyEmailOrPhoneNumberError(INVALID_PHONE_ERROR)
                 .completeVerify();
     }
 
@@ -27,7 +49,7 @@ public class LoginDashboard extends BaseTest {
     public void TC03_DB_LoginWithInvalidMailFormat() {
         new LoginPage(driver).navigate()
                 .performLogin("abc", PASSWORD)
-                .verifyEmailOrPhoneNumberError(INVALID_MAIL_ERROR_MESSAGE)
+                .verifyEmailOrPhoneNumberError(INVALID_MAIL_ERROR)
                 .completeVerify();
     }
 
@@ -35,7 +57,7 @@ public class LoginDashboard extends BaseTest {
     public void TC04_DB_LoginWithInvalidEmailAccount() {
         new LoginPage(driver).navigate()
                 .performLogin(generate.generateString(140) + "@nbobd.com", PASSWORD)
-                .verifyEmailOrPasswordIncorrectError(INVALID_MAIL_OR_PASSWORD_ERROR)
+                .verifyEmailOrPasswordIncorrectError(INVALID_CREDENTIALS_ERROR)
                 .completeVerify();
     }
 
@@ -43,14 +65,14 @@ public class LoginDashboard extends BaseTest {
     public void TC05_DB_LoginWithInvalidPhoneAccount() {
         new LoginPage(driver).navigate()
                 .performLogin(generate.generateNumber(13), PASSWORD)
-                .verifyEmailOrPasswordIncorrectError(INVALID_MAIL_OR_PASSWORD_ERROR)
+                .verifyEmailOrPasswordIncorrectError(INVALID_CREDENTIALS_ERROR)
                 .completeVerify();
     }
 
     @Test
     public void TC06_DB_LoginWithValidPhoneNumberAccount() {
         new LoginPage(driver).navigate()
-                .performLogin(PHONE, PASSWORD);
+                .performLogin(PHONE, PHONE_PASSWORD);
         new HomePage(driver).waitTillSpinnerDisappear().clickLogout();
     }
 
@@ -66,4 +88,12 @@ public class LoginDashboard extends BaseTest {
         new LoginPage(driver).navigate().performLoginWithFacebook("trangthuy9662@gmail.com", "123456aA@");     
         new HomePage(driver).waitTillSpinnerDisappear().clickLogout();
     }
+    
+    @Test
+    public void TC09_DB_StaffLoginWithValidMailAccount() {
+        new LoginPage(driver).navigate()
+        		.switchToStaffTab()
+                .performLogin("etrelwitdi@mailnesia.com", "uBMolFhj");
+        new HomePage(driver).waitTillSpinnerDisappear().clickLogout();
+    }    
 }
