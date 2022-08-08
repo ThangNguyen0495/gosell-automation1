c1216117f5dd3c7ea9fae453f41adc735e26ef0f
Tien Van <minhtienxoxo1@gmail.com>
2022-08-08 10:25:10 +0700

    Update TCs

--- src/test/java/LoginDashboard.java
+++ src/test/java/LoginDashboard.java
@@ -1,16 +1,22 @@
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import pages.Mailnesia;
 import pages.dashboard.LoginPage;
 import pages.dashboard.home.HomePage;
 import utilities.jsonFileUtility;
+import com.fasterxml.jackson.databind.JsonNode;
 
 public class LoginDashboard extends BaseTest {
 	
-    String MAIL = "qcgosell01@gmail.com";
-    String PASSWORD = "Psso12!@";
-    String PHONE = "0363530754";
-    String PHONE_PASSWORD = "Abc@12345";
-    
+    String MAIL;
+    String PASSWORD;
+    String PHONE;
+    String PHONE_PASSWORD;
+    String FACEBOOK;
+    String FACEBOOK_PASSWORD;
+    String STAFF;
+    String STAFF_PASSWORD;
     String BLANK_ERROR;
     String INVALID_MAIL_ERROR;
     String INVALID_PHONE_ERROR;
@@ -18,29 +24,53 @@ public class LoginDashboard extends BaseTest {
 	
     @BeforeClass
     public void readData() {
-    	try {
-			BLANK_ERROR = jsonFileUtility.readJsonFile("DBInfo.json").findValue("dashboard").findValue("emptyError").asText();
-			INVALID_MAIL_ERROR = jsonFileUtility.readJsonFile("DBInfo.json").findValue("dashboard").findValue("invalidMailFormat").asText();
-			INVALID_PHONE_ERROR = jsonFileUtility.readJsonFile("DBInfo.json").findValue("dashboard").findValue("invalidPhoneFormat").asText();
-			INVALID_CREDENTIALS_ERROR = jsonFileUtility.readJsonFile("DBInfo.json").findValue("dashboard").findValue("invalidCredentials").asText();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		JsonNode data = jsonFileUtility.readJsonFile("LoginInfo.json").findValue("dashboard");
+		
+		MAIL = data.findValue("seller").findValue("mail").findValue("username").asText();
+		PASSWORD = data.findValue("seller").findValue("mail").findValue("password").asText();
+		PHONE = data.findValue("seller").findValue("phone").findValue("username").asText();
+		PHONE_PASSWORD = data.findValue("seller").findValue("phone").findValue("password").asText();
+		FACEBOOK = data.findValue("seller").findValue("facebook").findValue("username").asText();
+		FACEBOOK_PASSWORD = data.findValue("seller").findValue("facebook").findValue("password").asText();
+		STAFF = data.findValue("staff").findValue("mail").findValue("username").asText();
+		STAFF_PASSWORD = data.findValue("staff").findValue("mail").findValue("password").asText();
+		
+		BLANK_ERROR = data.findValue("emptyError").asText();
+		INVALID_MAIL_ERROR = data.findValue("invalidMailFormat").asText();
+		INVALID_PHONE_ERROR = data.findValue("invalidPhoneFormat").asText();
+		INVALID_CREDENTIALS_ERROR = data.findValue("invalidCredentials").asText();
     }	    
     
     @Test
     public void TC01_DB_LoginWithAllFieldsLeftBlank() {
+    	// Username field is left empty.
         new LoginPage(driver).navigate()
-                .performLogin("", "")
+                .performLogin("", generate.generateNumber(9))
                 .verifyEmailOrPhoneNumberError(BLANK_ERROR)
-                .verifyPasswordError(BLANK_ERROR)
                 .completeVerify();
+        // Password field is left empty.
+        new LoginPage(driver).navigate()
+		        .performLogin(generate.generateNumber(10), "")
+		        .verifyPasswordError(BLANK_ERROR)
+		        .completeVerify();
+        // All fields are left empty.
+        new LoginPage(driver).navigate()
+		        .performLogin("", "")
+		        .verifyEmailOrPhoneNumberError(BLANK_ERROR)
+		        .verifyPasswordError(BLANK_ERROR)
+		        .completeVerify();
     }
 
     @Test
     public void TC02_DB_LoginWithInvalidPhoneFormat() {
+    	// Log in with a phone number consisting of 9 digits.
         new LoginPage(driver).navigate()
-                .performLogin("123", PASSWORD)
+                .performLogin(generate.generateNumber(9), generate.generateString(10))
+                .verifyEmailOrPhoneNumberError(INVALID_PHONE_ERROR)
+                .completeVerify();
+        // Log in with a phone number consisting of 14 digits.
+        new LoginPage(driver)
+        		.performLogin(generate.generateNumber(14), generate.generateString(10))
                 .verifyEmailOrPhoneNumberError(INVALID_PHONE_ERROR)
                 .completeVerify();
     }
@@ -48,52 +78,118 @@ public class LoginDashboard extends BaseTest {
     @Test
     public void TC03_DB_LoginWithInvalidMailFormat() {
         new LoginPage(driver).navigate()
-                .performLogin("abc", PASSWORD)
+                .performLogin(generate.generateString(10), generate.generateString(10))
                 .verifyEmailOrPhoneNumberError(INVALID_MAIL_ERROR)
                 .completeVerify();
     }
 
     @Test
-    public void TC04_DB_LoginWithInvalidEmailAccount() {
+    public void TC04_DB_LoginWithWrongEmailAccount() {
         new LoginPage(driver).navigate()
-                .performLogin(generate.generateString(140) + "@nbobd.com", PASSWORD)
+                .performLogin(generate.generateString(10) + "@nbobd.com", generate.generateString(10))
                 .verifyEmailOrPasswordIncorrectError(INVALID_CREDENTIALS_ERROR)
                 .completeVerify();
     }
 
     @Test
-    public void TC05_DB_LoginWithInvalidPhoneAccount() {
+    public void TC05_DB_LoginWithWrongPhoneAccount() {
         new LoginPage(driver).navigate()
-                .performLogin(generate.generateNumber(13), PASSWORD)
+                .performLogin(generate.generateNumber(13), generate.generateString(10))
                 .verifyEmailOrPasswordIncorrectError(INVALID_CREDENTIALS_ERROR)
                 .completeVerify();
     }
 
     @Test
-    public void TC06_DB_LoginWithValidPhoneNumberAccount() {
+    public void TC06_DB_LoginWithCorrectPhoneAccount() {
         new LoginPage(driver).navigate()
                 .performLogin(PHONE, PHONE_PASSWORD);
         new HomePage(driver).waitTillSpinnerDisappear().clickLogout();
     }
 
     @Test
-    public void TC07_DB_LoginWithValidMailAccount() {
+    public void TC07_DB_LoginWithCorrectMailAccount() {
         new LoginPage(driver).navigate()
                 .performLogin(MAIL, PASSWORD);
         new HomePage(driver).waitTillSpinnerDisappear().clickLogout();
     }
 
     @Test
-    public void TC08_DB_LoginWithFacebook() {
-        new LoginPage(driver).navigate().performLoginWithFacebook("trangthuy9662@gmail.com", "123456aA@");     
+    public void TC08_DB_LoginWithFacebook() throws InterruptedException {
+        new LoginPage(driver).navigate().performLoginWithFacebook(FACEBOOK, FACEBOOK_PASSWORD);   
         new HomePage(driver).waitTillSpinnerDisappear().clickLogout();
     }
-    
+
     @Test
-    public void TC09_DB_StaffLoginWithValidMailAccount() {
-        new LoginPage(driver).navigate()
-        		.switchToStaffTab()
-                .performLogin("etrelwitdi@mailnesia.com", "uBMolFhj");
-        new HomePage(driver).waitTillSpinnerDisappear().clickLogout();
+    public void TC09_DB_StaffLogin() {
+    	// Login with wrong credentials.
+    	new LoginPage(driver).navigate()
+    	.switchToStaffTab()
+    	.performLogin(STAFF, generate.generateString(10))
+    	.verifyEmailOrPasswordIncorrectError(INVALID_CREDENTIALS_ERROR)
+    	.completeVerify();
+    	// Login with correct credentials.
+    	new LoginPage(driver).navigate()
+    	.switchToStaffTab()
+    	.performLogin(STAFF, STAFF_PASSWORD);
+    	new HomePage(driver).waitTillSpinnerDisappear().clickLogout();
     }    
+    
+    //Don't run this test case. It should only be run in regression test.
+//    @Test
+    public void TC10_DB_SellerForgotEmailPassword() throws InterruptedException {
+    	String newPassword = PASSWORD + generate.generateNumber(4)+ "!";
+    	
+    	new LoginPage(driver).navigate()
+    	.clickForgotPassword()
+    	.inputEmailOrPhoneNumber(MAIL)
+    	.inputPassword(newPassword)
+    	.clickContinueOrConfirmBtn();
+    	
+    	// Get verification code from Mailnesia
+    	Thread.sleep(7000);
+    	commonAction.openNewTab(); // Open a new tab
+    	commonAction.switchToWindow(1); // Switch to the newly opened tab
+    	String verificationCode = new Mailnesia(driver).navigate(MAIL).getVerificationCode(); // Get verification code
+    	commonAction.closeTab(); // Close the newly opened tab
+    	commonAction.switchToWindow(0); // Switch back to the original tab
+    	
+    	new LoginPage(driver).inputVerificationCode(verificationCode)
+    	.clickContinueOrConfirmBtn();
+    	new HomePage(driver).clickLogout();
+    	
+    	// Re-login with new password
+    	new LoginPage(driver).navigate().performLogin(MAIL, newPassword);
+    	new HomePage(driver).clickLogout();
+    }
+    
+    //Don't run this test case. It should only be run in regression test.
+//    @Test
+    public void TC11_DB_StaffForgotPassword() throws InterruptedException {
+    	String newPassword = STAFF_PASSWORD + generate.generateNumber(4)+ "!";
+    	
+        new LoginPage(driver).navigate()
+		.switchToStaffTab()
+		.clickForgotPassword()
+        .inputEmailOrPhoneNumber(STAFF)
+        .inputPassword(newPassword)
+        .clickContinueOrConfirmBtn();
+        
+    	// Get verification code from Mailnesia
+        Thread.sleep(7000);
+    	commonAction.openNewTab(); // Open a new tab
+    	commonAction.switchToWindow(1); // Switch to the newly opened tab
+    	String verificationCode = new Mailnesia(driver).navigate(STAFF).getVerificationCode(); // Get verification code
+    	commonAction.closeTab(); // Close the newly opened tab
+    	commonAction.switchToWindow(0); // Switch back to the original tab
+    	
+    	new LoginPage(driver).inputVerificationCode(verificationCode)
+    	.clickContinueOrConfirmBtn();
+    	new HomePage(driver).clickLogout();
+
+    	// Re-login with new password
+    	new LoginPage(driver).navigate()
+    	.switchToStaffTab()
+    	.performLogin(STAFF, newPassword);
+    	new HomePage(driver).clickLogout();
+    }
 }
--- src/test/java/LoginStorefront.java
+++ src/test/java/LoginStorefront.java
@@ -1,18 +1,147 @@
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import pages.Mailnesia;
+import pages.storefront.HeaderSF;
 import pages.storefront.LoginPage;
+import utilities.jsonFileUtility;
 
 public class LoginStorefront extends BaseTest {
+
+	String MAIL;
+	String PASSWORD;
+	String PHONE;
+	String PHONE_PASSWORD;
+	String BLANK_USERNAME_ERROR;
+	String BLANK_PASSWORD_ERROR;
+	String INVALID_USERNAME_ERROR;
+	String INVALID_CREDENTIALS_ERROR;
+
+	@BeforeClass
+	public void readData() {
+		JsonNode data = jsonFileUtility.readJsonFile("LoginInfo.json").findValue("storefront");
+
+		MAIL = data.findValue("buyer").findValue("mail").findValue("username").asText();
+		PASSWORD = data.findValue("buyer").findValue("mail").findValue("password").asText();
+		PHONE = data.findValue("buyer").findValue("phone").findValue("username").asText();
+		PHONE_PASSWORD = data.findValue("buyer").findValue("phone").findValue("password").asText();
+
+		BLANK_USERNAME_ERROR = data.findValue("emptyUsernameError").asText();
+		BLANK_PASSWORD_ERROR = data.findValue("emptyPasswordError").asText();
+		INVALID_USERNAME_ERROR = data.findValue("invalidUsernameFormat").asText();
+		INVALID_CREDENTIALS_ERROR = data.findValue("invalidCredentials").asText();
+	}
+
+	@Test
+	public void TC01_SF_LoginWithAllFieldsLeftBlank() {
+		new LoginPage(driver).navigate()
+		.performLogin("", generate.generateNumber(9))
+		.verifyEmailOrPhoneNumberError(BLANK_USERNAME_ERROR)
+		.completeVerify();
+		// Password field is left empty.
+		new LoginPage(driver).navigate()
+		.performLogin(generate.generateNumber(9), "")
+		.verifyPasswordError(BLANK_PASSWORD_ERROR)
+		.completeVerify();
+		// All fields are left empty.
+		new LoginPage(driver).navigate()
+		.performLogin("", "")
+		.verifyEmailOrPhoneNumberError(BLANK_USERNAME_ERROR)
+		.verifyPasswordError(BLANK_PASSWORD_ERROR)
+		.completeVerify();
+	}
+
+    @Test
+    public void TC02_SF_LoginWithInvalidPhoneFormat() {
+    	// Log in with a phone number consisting of 7 digits.
+        new LoginPage(driver).navigate()
+                .performLogin(generate.generateNumber(7), generate.generateString(10))
+                .verifyEmailOrPhoneNumberError(INVALID_USERNAME_ERROR)
+                .completeVerify();
+        // Log in with a phone number consisting of 16 digits.
+        new LoginPage(driver).navigate()
+        		.performLogin(generate.generateNumber(16), generate.generateString(10))
+                .verifyEmailOrPhoneNumberError(INVALID_USERNAME_ERROR)
+                .completeVerify();
+    }	
+
+    @Test
+    public void TC03_SF_LoginWithInvalidMailFormat() {
+        new LoginPage(driver).navigate()
+                .performLogin(generate.generateString(10), generate.generateString(10))
+                .verifyEmailOrPhoneNumberError(INVALID_USERNAME_ERROR)
+                .completeVerify();
+    }    
+ 
+    @Test
+    public void TC04_SF_LoginWithWrongEmailAccount() {
+        new LoginPage(driver).navigate()
+                .performLogin(generate.generateString(10) + "@nbobd.com", generate.generateString(10))
+                .verifyEmailOrPasswordIncorrectError(INVALID_CREDENTIALS_ERROR)
+                .completeVerify();
+    }    
+
+    @Test
+    public void TC05_SF_LoginWithWrongPhoneAccount() {
+        new LoginPage(driver).navigate()
+                .performLogin(generate.generateNumber(13), generate.generateString(10))
+                .verifyEmailOrPasswordIncorrectError(INVALID_CREDENTIALS_ERROR)
+                .completeVerify();
+    }    
+
+    @Test
+    public void TC06_SF_LoginWithCorrectPhoneAccount() throws InterruptedException {
+        new LoginPage(driver).navigate()
+                .performLogin(PHONE, PHONE_PASSWORD);
+        Thread.sleep(1000);
+        new LoginPage(driver).clickUserInfoIcon();
+        new HeaderSF(driver).clickLogout();
+    }    
+
+    @Test
+    public void TC07_SF_LoginWithCorrectMailAccount() throws InterruptedException {
+        new LoginPage(driver).navigate()
+                .performLogin(MAIL, PASSWORD);
+        Thread.sleep(1000);
+        new LoginPage(driver).clickUserInfoIcon();
+        new HeaderSF(driver).clickLogout();
+    }    
+
+  //Don't run this test case. It should only be run in regression test.
+//  @Test
+  public void TC10_SF_ForgotPassword() throws InterruptedException {
+  	String newPassword = PASSWORD + "@" + generate.generateNumber(3);
+  	
+  	new LoginPage(driver).navigate()
+  	.clickUserInfoIcon()
+  	.clickLoginIcon()
+  	.clickForgotPassword()
+  	.inputUsernameForgot(MAIL)
+  	.clickContinueBtn()
+  	.inputPasswordForgot(newPassword);
+  	
+  	// Get verification code from Mailnesia
+  	Thread.sleep(7000);
+  	commonAction.openNewTab(); // Open a new tab
+  	commonAction.switchToWindow(1); // Switch to the newly opened tab
+  	String verificationCode = new Mailnesia(driver).navigate(MAIL).getVerificationCode(); // Get verification code
+  	commonAction.closeTab(); // Close the newly opened tab
+  	commonAction.switchToWindow(0); // Switch back to the original tab
+  	
+	new LoginPage(driver).inputVerificationCode(verificationCode)
+	.clickConfirmBtn();
+    Thread.sleep(1000);
+    new LoginPage(driver).clickUserInfoIcon();
+    new HeaderSF(driver).clickLogout();
     
-    @Test
-    public void TC06_SF_LoginWithValidPhoneNumberAccount() {
-        new LoginPage(driver).navigate()
-        .performLogin("0856256079", "12345678x@X");
-    }
-
-    @Test
-    public void TC07_SF_LoginWithValidMailAccount() {
-        new LoginPage(driver).navigate()
-        .performLogin("buyertest@mailnesia.com", "fortesting!4");
-    }
-
+	// Re-login with new password
+	new LoginPage(driver).navigate()
+	.performLogin(MAIL, newPassword);
+	Thread.sleep(1000);
+    new LoginPage(driver).clickUserInfoIcon();
+    new HeaderSF(driver).clickLogout();
+  }
+  
 }
